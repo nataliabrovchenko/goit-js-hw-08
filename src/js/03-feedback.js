e785119cbf115f079e343444984f37a24d2d63fc
nataliabrovchenko <nats.brovchenko@gmail.com>
2022-11-17 18:15:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const form = document.querySelector(`.feedback-form`);
+const input = document.querySelector(`.email message`);
 
+form.addEventListener(`submit`, onFormSubmit);
+input.addEventListener(`input`, onInput);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(`отправка`)
+}
