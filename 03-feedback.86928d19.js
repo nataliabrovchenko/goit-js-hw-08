2db093b2b2336ad96a6236ece80a2d542faa9ff5
nataliabrovchenko <108325814+nataliabrovchenko@users.noreply.github.com>
2022-11-18 13:05:56 +0000

    Deploying to gh-pages from @ nataliabrovchenko/goit-js-hw-08@e785119cbf115f079e343444984f37a24d2d63fc 🚀

---
+++ 03-feedback.86928d19.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form"),t=document.querySelector(".email message");e.addEventListener("submit",(function(e){e.preventDefault(),console.log("отправка")})),t.addEventListener("input",onInput);
+//# sourceMappingURL=03-feedback.86928d19.js.map
