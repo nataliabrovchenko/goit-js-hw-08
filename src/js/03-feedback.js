dfc66151cb0cd9f5de65ef31201fda7aec96ea7c
nataliabrovchenko <nats.brovchenko@gmail.com>
2022-11-19 12:06:31 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,20 +3,36 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = `feedback-form-state`;
 
 const form = document.querySelector(`.feedback-form`);
-const textarea = document.querySelector(`.feedback-form textarea`);
 
+form.addEventListener(`input`, throttle(onInput, 500));
 form.addEventListener(`submit`, onFormSubmit);
-textarea.addEventListener(`input`, throttle(onInput, 500));
+form.addEventListener(`load`, onLoad);
 
 populateMessage();
 
 function onInput(event) {
-    const message = event.currentTarget.value;
-    localStorage.setItem(STORAGE_KEY, message);
-} 
+    event.preventDefault();
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));    
+  }
+
+function onLoad (event) {
+    event.preventDefault();
+    const textDataForm = localStorage.getItem(STORAGE_KEY);
+    const dataForm = JSON.parse(textDataForm);
+    const { email, message } = dataForm;
+    form.elements.email.value = email;
+    form.elements.message.value = message;
+}
 
 function onFormSubmit(event) {
     event.preventDefault();
+    const {
+        email,
+        message
+    } = event.currentTarget;
+    console.log({email: email.value, message: message.value})
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
@@ -25,7 +41,7 @@ function populateMessage() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
     
     if(savedMessage) {
-        textarea.value = savedMessage;
+        form.value = savedMessage;
     }
 }
 
